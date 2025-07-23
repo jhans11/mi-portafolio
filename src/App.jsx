8a542545b3b5981f79427863cb763a9bd578ee4d
jhans11 <deibyok@gmail.com>
2025-07-23 01:18:40 -0500

    🚀 Proyecto base React con Vite

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "4rem" }}>
+      <h1>¡Hola, soy Jhans! 💻</h1>
+      <p>Este es el inicio de mi portafolio profesional con React + Vite 🚀</p>
+    </div>
+  );
+}
+
+export default App;
